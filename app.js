aaeb4b9d25e1f2aac768a93eabf4ea74104f0e02
AdilSami11 <adilsamikhan2005@gmail.com>
2026-08-30 21:08:44 +0500

    app.js file as been updated

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
 const express = require('express');
 const app = express();
+const cookieParser = require('cookie-parser');
+const path = require('path');
+
+// middleWares :
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.send('Main Route');
+});
+
+app.listen(3000, () => {
+  console.log('server is running....');
+});
