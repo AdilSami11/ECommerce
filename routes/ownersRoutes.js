17a8fec826f772ba361f954112073827b1182ac0
AdilSami11 <adilsamikhan2005@gmail.com>
2026-09-02 20:20:47 +0500

    Basic Additions and Fixitations.

--- routes/ownersRoutes.js
+++ routes/ownersRoutes.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const ownerModel = require('../models/owner-Model');
 
 router.get('/', (req, res) => {
   res.send('Hey it Owner Root Route....');
 });
+
+if (process.env.NODE_ENV === 'development') {
+  router.post('/create', (req, res) => {
+    res.send('Hey Create Route is Working..');
+  });
+}
 
 module.exports = router;
